d0dcf99cca22d6a3bff4b8cccfd61f0eee956521
Manuel Flandorfer <mflandor@greenpeace.org>
2025-02-20 13:56:37 +0100

    GP-47302 Add managed entity for contract cancellation parent tag

---
+++ managed/Tag_contract_cancellation.mgd.php
@@ -0,0 +1,28 @@
+<?php
+
+use CRM_Contract_ExtensionUtil as E;
+
+return [
+  [
+    'name' => 'Tag_contract_cancellation',
+    'entity' => 'Tag',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'contract_cancellation',
+        'label' => E::ts('Contract Cancellation'),
+        'description' => E::ts('Tag Set for contract cancellation activities'),
+        'is_reserved' => TRUE,
+        'is_tagset' => TRUE,
+        'used_for' => [
+          'civicrm_activity',
+        ],
+      ],
+      'match' => [
+        'name',
+      ],
+    ],
+  ],
+];
