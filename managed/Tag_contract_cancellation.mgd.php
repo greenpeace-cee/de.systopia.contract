c0e2d5e44a1aaf8f0bb8a9dd9453f8501fb70008
Manuel Flandorfer <mflandor@greenpeace.org>
2025-02-26 18:14:13 +0100

    GP-47302 Make contract_cancellation a regular tag (not a tag set)

--- managed/Tag_contract_cancellation.mgd.php
+++ managed/Tag_contract_cancellation.mgd.php
@@ -15,7 +15,7 @@ return [
         'label' => E::ts('Contract Cancellation'),
         'description' => E::ts('Tag Set for contract cancellation activities'),
         'is_reserved' => TRUE,
-        'is_tagset' => TRUE,
+        'is_tagset' => FALSE,
         'used_for' => [
           'civicrm_activity',
         ],
